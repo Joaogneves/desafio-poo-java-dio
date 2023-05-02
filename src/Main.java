ffaf35f7977bd54dfbda9b9c39254683245aed0f
Joao <joaogabriel443@gmail.com>
2023-05-02 08:46:22 -0300

    Adicao das classes curso e mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Curso curso1 = new Curso("Java", "Curso de java para iniciantes...", 8);
+        Curso curso2 = new Curso("Kotlin", "Curso de kotlin para iniciantes...", 8);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Mentoria mentoria = new Mentoria("Mentoria de java", "mentoria java...", LocalDate.now());
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,61 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.Objects;
+
+public class Curso {
+    private String nome;
+    private String descricao;
+    private Integer cargaHoraria;
+
+    public Curso(String nome, String descricao, Integer cargaHoraria) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Curso curso = (Curso) o;
+        return Objects.equals(nome, curso.nome) && Objects.equals(descricao, curso.descricao) && Objects.equals(cargaHoraria, curso.cargaHoraria);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, cargaHoraria);
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
